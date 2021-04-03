1b4c6c717cbb1b1ba2209f2bbec68528b30347d2
ji-yeon224 <shjh0107@gmail.com>
2021-04-03 16:17:46 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,49 @@
+package com.example.board.controller;
+
+import com.example.board.dto.BoardDto;
+import com.example.board.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class BoardController {
+
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
+
+
+    @GetMapping("/")
+    public String list(Model model){
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("boardList", boardDtoList);
+
+        return  "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write(){
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto){
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long id, Model model){
+        BoardDto boardDto = boardService.getPost(id);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+}
